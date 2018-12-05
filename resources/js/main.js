9110acd14c410a909e0d6f03a93a7e8101d25960
Oren Fix <orenfix@gmail.com>
2018-12-05 03:01:34 -0800

    First draft of website

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-	$('main').css('visibility','visible').css('opacity',0).animate({ opacity:1 },800,'linear');
+	$('.photos').css('opacity',0).animate({ opacity:1 },1000,'linear');
+	
+	$('.mute').click( function (){
+    	if( $('.video').prop('muted') ) {
+        	$('.video').prop('muted',false);
+        	$('.mute').html('Mute');
+    	} else {
+    		$('.video').prop('muted',true);
+    		$('.mute').html('Unmute');
+    	}
+	});
 });
