221fd418da1a3a75dd4bed43e10e085fafcbd4e8
Oren Fix <orenfix@gmail.com>
2018-12-05 23:20:43 -0800

    Move video from MP4 to YouTube

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){
 	$('.photos').css('opacity',0).animate({ opacity:1 },1000,'linear');
-	
-	$('.mute').click( function (){
-    	if( $('.video').prop('muted') ) {
-        	$('.video').prop('muted',false);
-        	$('.mute').html('Mute');
-    	} else {
-    		$('.video').prop('muted',true);
-    		$('.mute').html('Unmute');
-    	}
-	});
-});
+ });
+
+//YouTube API
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    
+    var player;
+
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+            width: 300,
+            height: 168.7,
+            playerVars: { 
+                'controls': 0, 
+                'modestbranding': 1,
+                'loop': 1,
+                'autoplay': 1,
+                'playlist': 'prjAQqgqyJE',
+                'playsinline': 1,
+                'enablejsapi': 1,
+                'origin': 'https://jayandronit.com'
+                },
+            videoId: 'prjAQqgqyJE'
+            }
+         )};  
+      
+   
+
+         
