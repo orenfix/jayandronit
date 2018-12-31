36a35f7a7b0d0ca2328643dd0e33335c087d382e
Oren Fix <orenfix@gmail.com>
2018-12-31 00:44:04 -0800

    adding mawwiage

--- resources/js/main.js
+++ resources/js/main.js
@@ -34,33 +34,6 @@ $(document).ready(function(){
         }
     });
  });
-
-//YouTube API
-    var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    
-    var player;
-
-    function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-            width: 300,
-            height: 168.7,
-            playerVars: { 
-                'controls': 0, 
-                'modestbranding': 1,
-                'loop': 1,
-                'autoplay': 1,
-                'playlist': 'prjAQqgqyJE',
-                'playsinline': 1,
-                'enablejsapi': 1,
-                'origin': 'https://jayandronit.com'
-                },
-            videoId: 'prjAQqgqyJE'
-            }
-         )};  
-      
    
 
          
