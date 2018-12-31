67f17e2a8813e780322e2e9ec0744d3fe7b93df3
Oren Fix <orenfix@gmail.com>
2018-12-30 20:54:34 -0800

    New website

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,38 @@
 $(document).ready(function(){
-	$('.photos').css('opacity',0).animate({ opacity:1 },1000,'linear');
+	$('main').css('opacity',0).animate({ opacity:1 },600,'linear');
+    $('.menu').click(function() {
+        $('nav ul').slideToggle();
+    });
+
+    $(window).resize(function() {
+        var windowWidth = $(window).width();
+        if ( windowWidth > 970 ) {
+            $('nav ul').css('display','flex');
+        } else {
+            $('nav ul').slideUp(0);
+        }
+    });
+
+    $('.mawwiage').mouseover (function() {
+        if ( $('audio')[0].ended ) {
+            $('.play').removeClass('fa-pause').addClass('fa-play');
+        } else if ( $('audio')[0].paused ) {
+            $('.play').removeClass('fa-pause').addClass('fa-play');
+        } else {
+            $('.play').removeClass('fa-play').addClass('fa-pause');
+        }
+    });
+
+    $('.mawwiage').click(function() {
+        if ( $('audio')[0].paused ) {
+            $('audio')[0].play();
+            $('.play').removeClass('fa-play').addClass('fa-pause');
+        } else {
+            $('audio')[0].pause();
+            $('audio')[0].currentTime=0;
+            $('.play').removeClass('fa-pause').addClass('fa-play');
+        }
+    });
  });
 
 //YouTube API
